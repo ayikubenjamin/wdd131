541aa02a4eb50ded0dc34eb05d2192e7710bdcb2
ayikubenjamin <ayikutetteh70@gmail.com>
2025-10-01 21:16:41 +0000

    correction of formula

--- scripts/place.js
+++ scripts/place.js
@@ -14,7 +14,7 @@ windSpeed.textContent = `Wind Speed: ${wind} km/h`
 
 function calculateWindChill(temp, wind){
     if (temp <= 10 && wind > 4.8){
-        const result = 13.12 + 0.6215*temp - (11.37*wind)**0.16 + 0.3965*temp * wind**0.16
+        const result = 13.12 + 0.6215 * temp - 11.37 * (wind ** 0.16) + 0.3965 * temp * (wind ** 0.16)
         return Math.round(result * 10)/10
     } else {
         console.log('N/A')
@@ -22,4 +22,4 @@ function calculateWindChill(temp, wind){
 }
 
 const windchill = document.querySelector('#windchill')
-windchill.textContent = `Wind Chill :${calculateWindChill(temp, wind)}`
+windchill.textContent = `Wind Chill :${calculateWindChill(temp, wind)} ℃`
