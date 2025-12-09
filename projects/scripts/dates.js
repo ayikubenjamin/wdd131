4818be9bbd50592661d1e7bf2c1990f29767e491
ayikubenjamin <ayikutetteh70@gmail.com>
2025-12-09 23:32:39 +0000

    final work

--- projects/scripts/dates.js
+++ projects/scripts/dates.js
@@ -4,3 +4,13 @@ const today = new Date()
 const datetime = today.toLocaleString("en-Us")
 currentyear.innerHTML = today.getFullYear()
 lastmodified.innerHTML = `Last Modified : ${datetime}`
+
+
+
+const hamburger = document.querySelector('#nav-button');
+const nav = document.querySelector('nav');
+
+hamburger.addEventListener('click', () => {
+    nav.classList.toggle('open');
+    hamburger.classList.toggle('open');
+});
